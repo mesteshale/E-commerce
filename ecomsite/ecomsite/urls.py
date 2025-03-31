006a4482f2b30d5e6003877d0e2659cc37450959
mesteshale <mesteshale@gmail.com>
2025-03-31 23:22:54 +0300

    work on index and detail view

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
+    path('detail/<int:id>/',views.detail, name = 'detail')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files during development
--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
+
+def index(request):
+    product_objects = Product.objects.all()
+    #search code
+    product_name = request.GET.get('product_name')
+    if product_name != '' and product_name is not None:
+        product_objects = Product.objects.filter(title__icontains=product_name)
+    #pagination code
+    paginator = Paginator(product_objects, 8)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
+    return render(request, 'shop/index.html', {'product_objects':product_objects})
+
+def detail(request, id):
+    product_object = Product.objects.get(id=id)
+    related_products = Product.objects.filter(category=product_object.category).exclude(id=id)[:4]
+    return render(request, 'shop/detail.html', {'product_object':product_object, 'related_products':related_products})
